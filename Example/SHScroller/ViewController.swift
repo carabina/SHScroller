455be272b0a3538e32fad906996395c87b437889
Sho Miyamoto <sho@MBA.local>
2016-08-31 00:29:26 +0900

    First Commit

--- Example/SHScroller/ViewController.swift
+++ Example/SHScroller/ViewController.swift
@@ -9,15 +9,49 @@
 import UIKit
 import SHScroller
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-         
-        // Do any additional setup after loading the view, typically from a nib.
+        var viewControllers: [UIViewController] = []
+        
+        // Create 5 ViewControllers with different background colors
+        for i in 0 ..< 5 {
+            let controller = UIViewController()
+            controller.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarSystemItem.MostRecent, tag: i)
+            controller.view.backgroundColor = UIColor(hue: CGFloat(i)/5, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+            viewControllers.append(controller)
+        }
+        
+        self.setViewControllers(viewControllers, animated: false)
+
+        
+        
+        //MARK: Initialize SHScroller
+        let scroller = SHScroller(superView: self.view)
+        
+        //MARK: You can also designate division number and inset like this
+//        let scroller = SHScroller(superView: self.view, division: <#T##Int#>, inset: <#T##CGFloat#>)
+        
+        self.view.addSubview(scroller)
+        
+        //MARK: Associate your funcition with SHScroller
+        scroller.addTarget(self, action: #selector(ViewController.changed), forControlEvents: .ValueChanged)
+        
     }
+    
+    
+    // Associated function
+    func changed(scroller: SHScroller) {
+        
+        //MARK: You can access only this propety named delta, which can only be read.
+        //MARK: Delta propety takes either 1 or -1
+        self.selectedIndex += scroller.delta
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
